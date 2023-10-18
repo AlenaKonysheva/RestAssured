c4a626e9568b756de02c61898b34e9c0159015ea
AlenaKonysheva <auslonceva@ya.ru>
2023-10-18 20:59:33 +0200

    add tests for create user method

--- src/test/java/CreateUserDTOTest.java
+++ src/test/java/CreateUserDTOTest.java
@@ -7,7 +7,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class CreateUserDTOTest {
     @Test
-    public void checkCreateUser() {
+    public void checkCreateUserAllFields() {
         CreateUserAPI createUserAPI = new CreateUserAPI();
         CreateUserDTO newUserAllFields = CreateUserDTO.builder()
                 .id(300l)
@@ -21,8 +21,34 @@ public class CreateUserDTOTest {
                 .build();
         createUserAPI.createUser(newUserAllFields)
                 .statusCode(HttpStatus.SC_OK)
-                .body("code",equalTo(200))
-                .body("type",equalTo("unknown"))
-                .body("message",equalTo("300"));
+                .body("code", equalTo(200))
+                .body("type", equalTo("unknown"))
+                .body("message", equalTo("300"));
+    }
+
+    @Test
+    public void checkCreateUserTwoFields() {
+        CreateUserAPI createUserAPI = new CreateUserAPI();
+        CreateUserDTO newUserAllFields = CreateUserDTO.builder()
+                .id(100l)
+                .firstName("Test")
+                .build();
+        createUserAPI.createUser(newUserAllFields)
+                .statusCode(HttpStatus.SC_OK)
+                .body("code", equalTo(200))
+                .body("type", equalTo("unknown"))
+                .body("message", equalTo("100"));
+    }
+
+    @Test
+    public void checkCreateUserWithoutFields() {
+        CreateUserAPI createUserAPI = new CreateUserAPI();
+        CreateUserDTO newUserAllFields = CreateUserDTO.builder()
+                .build();
+        createUserAPI.createUser(newUserAllFields)
+                .statusCode(HttpStatus.SC_OK)
+                .body("code", equalTo(200))
+                .body("type", equalTo("unknown"))
+                .body("message", equalTo("0"));
     }
 }
